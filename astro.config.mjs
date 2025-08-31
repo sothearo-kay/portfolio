53afbd3e7e2d5393bddd9d0bdc6d3cb025cdcd38
sothearo-kay <sothearo.kay@techostartup.center>
2025-08-31 18:08:53 +0700

    feat(pages): add manga page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,14 @@
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  image: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mangadex.org",
+        pathname: "/covers/**",
+      },
+    ],
+  },
+})
