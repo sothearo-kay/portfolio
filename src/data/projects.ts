15322d9a8e0423ab98a1b44b7565066be505682e
sothearo-kay <sothearo.kay@techostartup.center>
2025-10-21 17:02:18 +0700

    feat(projects): add quickkeys

---
+++ src/data/projects.ts
@@ -0,0 +1,15 @@
+export interface Project {
+  title: string
+  description: string
+  previewUrl?: string
+  githubUrl?: string
+}
+
+export const projectList: Project[] = [
+  {
+    title: "QuickKeys",
+    description: "A minimalist typing speed test application with real-time performance metrics and user tracking capabilities.",
+    previewUrl: "https://wpm.sothearo.dev",
+    githubUrl: "https://github.com/sothearo-kay/quickkeys",
+  },
+]
